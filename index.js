fc48eb50a8d3573424e2ac052cee9ae4ee2c77f8
lff-gd <152182888+lff-gd@users.noreply.github.com>
2023-11-27 15:05:24 +0100

    Update index html and js

---
+++ index.js
@@ -0,0 +1,25 @@
+const parentContainers = document.querySelectorAll('.container-extra-info');
+
+parentContainers.forEach(container => {
+    container.addEventListener('click', event => {
+        const current = event.target;
+        const isReadMoreBtn = current.className.includes('read-more-btn');
+
+        if (!isReadMoreBtn) {
+            window.alert("****ERROR: isReadMoreBtn");
+            return;
+        }
+
+        current.textContent = current.textContent.includes('Saber más') ? "Ocultar" : "Saber más";
+
+        const currentBox = current.parentElement.childNodes[1];
+        if (!currentBox) {
+            window.alert("****ERROR: currentBox");
+            return;
+        }
+
+        if (currentBox.style.display != 'none') { currentBox.style.display = 'none'; }
+        else { currentBox.style.display = ''; }
+
+    })
+});
